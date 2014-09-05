b3f20e9a3437fec92a2e0cea040c07710ae3077a
sindrenj <sindre@sindrenj.net>
2014-09-05 14:44:34 +0200

    Formaterte GamesController.php

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -1,42 +1,38 @@
 <?php
 
- /**
-  * @file
-  * Contains \Drupal\gamesregister\Controller\GamesController.
+/**
+ * @file
+ * Contains \Drupal\gamesregister\Controller\GamesController.
  */
- 
- namespace Drupal\GamesRegister\Controller;
- 
- use Drupal\Core\Controller\ControllerBase;
- use Symfony\Component\DependencyInjection\ContainerInterface;
- 
- class GamesController extends ControllerBase {
- 	
-	public function games() {
-		return "Games!";
-	}
-	
-	public function register() {
-		return "Register games!";
-	}
-	
-	public function delete() {
-		return "Delete games!";
-	}
-	
-	public function update() {
-		return "update games!";
-	}
-	
-	/**
-   * {@inheritdoc}
-   */
-  public function access(AccountInterface $account) {
-    return $account->hasPermission('access content');
-  }  
- }
 
- 
- 
-	 
-	
+namespace Drupal\GamesRegister\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class GamesController extends ControllerBase {
+
+    public function games() {
+        return "Games!";
+    }
+
+    public function register() {
+        return "Register games!";
+    }
+
+    public function delete() {
+        return "Delete games!";
+    }
+
+    public function update() {
+        return "update games!";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function access(AccountInterface $account) {
+        return $account->hasPermission('access content');
+    }
+
+}
